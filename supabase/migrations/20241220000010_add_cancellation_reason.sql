5a34038630061c3c095ac68ba26075dccd702b76
parkmyoungjin <pmj628@naver.com>
2025-07-10 17:21:13 +0900

    Delete supabase directory

--- supabase/migrations/20241220000010_add_cancellation_reason.sql
+++
@@ -1,7 +0,0 @@
--- Add cancellation_reason column to reservations table
-ALTER TABLE reservations ADD COLUMN cancellation_reason TEXT;
-
--- Set default reason for existing cancelled reservations
-UPDATE reservations 
-SET cancellation_reason = '(이전 취소 기록)'
-WHERE status = 'cancelled' AND cancellation_reason IS NULL; 
